ecb1efbcd1732a7882e967144e86fdcda34859d2
ArslanSat <131513937+ArslanSat@users.noreply.github.com>
2023-06-13 21:41:50 +0500

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+
+// 1 =====
+
+for(const slide of slides) {
+    slide.addEventListener('click', () => {
+
+        clearActiveClasses();
+
+        slide.classList.add('active');
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+
+}
